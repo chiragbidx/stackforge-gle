516b75c8bf1dfd9340a96d7b05d7bddf556c9fb5
chiragbidx <chirag@bidx.ai>
2026-03-09 10:18:05 +0530

    Fix AgentActionPanel barrel export to use named export

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,4 @@
-export { default as AgentActionPanel } from "./AgentActionPanel";
+export { AgentActionPanel } from "./AgentActionPanel";
 export { default as ErrorReporter } from "./ErrorReporter";
 export { default as JobPostForm } from "./JobPostForm";
 export { default as JobSearchPanel } from "./JobSearchPanel";
